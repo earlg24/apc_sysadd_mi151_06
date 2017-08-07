f668c66101affaec7f1b2ae1ac1065c182457dd4
earlg24 <earlpetergonzaga2@gmail.com>
2017-08-07 11:16:39 +0800

    created entry-confirm.php

--- practice/eogonzaga/basic/views/site/entry-confirm.php
+++ practice/eogonzaga/basic/views/site/entry-confirm.php
@@ -1,8 +1,9 @@
 <?php
 use yii\helpers\Html;
 ?>
-<p>You have entered the following information:</p>
+<p> You have entered the following information:</p>
+
 <ul>
-<li><label>Name</label>: <?= Html::encode($model->name) ?></li>
-<li><label>Email</label>: <?= Html::encode($model->email) ?></li>
+	<li><label>Name</label>: <?= Html::encode($model->name) ?></li>
+	<li><label>Email</label>: <?= Html::encode($model->email) ?></li>
 </ul>
