cafc351c0108e0d73e0852638cf081f1c334eaef
jmfamador <famador.jimmy23@gmail.com>
2017-08-26 17:46:38 +0800

    edited index.php body

--- Application/basic/views/site/about.php
+++ Application/basic/views/site/about.php
@@ -11,7 +11,10 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        This is the About page. You may modify the following file to customize its content:
+        The Unity Housekeeping system contains the Hotel Room Cleaning Checklist 
+		Module that revolves around tracking the cleaning and checking of each hotel
+		room. The Unity Housekeeping system will have an automated check list that lets 
+		the user track their time of work which can help them be motivated to be more productive.
     </p>
 
     <code><?= __FILE__ ?></code>
--- Application/basic/views/site/index.php
+++ Application/basic/views/site/index.php
@@ -2,14 +2,14 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Unity Housekeeping';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
         <h1>Unity Housekeeping</h1>
 
-        <p class="lead">Organize room maintenance tasks.</p>
+        <h3>Organize room maintenance tasks.</h3>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Try it!</a></p>
     </div>
@@ -28,12 +28,11 @@ $this->title = 'My Yii Application';
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Purpose</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>The system aims to help housekeeping staff with task management, room checking and room cleaning.
+				The housekeeping inspector will be assigning the housekeeper to clean the room and the inspector will 
+				each room that the housekeeper maintained. </p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
