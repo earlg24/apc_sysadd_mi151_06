2737c6a4cc65e034044b677527a76b56f665c9da
tubetita <tyronnebetita@gmail.com>
2017-08-29 14:33:09 +0800

    modified ChecklistCategory.php

--- Application/basic/models/ChecklistCategory.php
+++ Application/basic/models/ChecklistCategory.php
@@ -5,7 +5,7 @@ namespace app\models;
 use Yii;
 
 /**
- * This is the model class for table "checklist_category".
+ * This is the model  class for table "checklist_category".
  *
  * @property integer $id
  * @property string $checklist_category_name
