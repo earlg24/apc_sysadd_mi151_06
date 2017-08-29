6330613a9f39d9c1aff78c935f875b161d816dd7
ryngrcia <ryngrcia@gmail.com>
2017-08-29 14:45:24 +0800

    modified HousekeepingLogDetails.php

--- Application/basic/models/HousekeepingLogDetails.php
+++ Application/basic/models/HousekeepingLogDetails.php
@@ -11,8 +11,12 @@ use Yii;
  * @property string $housekeeping_log_details_checklist
  * @property string $housekeeping_log_details_status
  * @property string $housekeeping_log_details_timecompleted
+ * @property integer $housekeeping_log_id
+ * @property integer $housekeeping_log_room_id
+ * @property integer $housekeeping_log_room_room_type_id
+ * @property integer $housekeeping_log_employee_id
  *
- * @property HousekeepingLog[] $housekeepingLogs
+ * @property HousekeepingLog $housekeepingLog
  */
 class HousekeepingLogDetails extends \yii\db\ActiveRecord
 {
@@ -30,8 +34,12 @@ class HousekeepingLogDetails extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
+            [['housekeeping_log_details_status'], 'string'],
             [['housekeeping_log_details_timecompleted'], 'safe'],
-            [['housekeeping_log_details_checklist', 'housekeeping_log_details_status'], 'string', 'max' => 45],
+            [['housekeeping_log_id', 'housekeeping_log_room_id', 'housekeeping_log_room_room_type_id', 'housekeeping_log_employee_id'], 'required'],
+            [['housekeeping_log_id', 'housekeeping_log_room_id', 'housekeeping_log_room_room_type_id', 'housekeeping_log_employee_id'], 'integer'],
+            [['housekeeping_log_details_checklist'], 'string', 'max' => 60],
+            [['housekeeping_log_id', 'housekeeping_log_room_id', 'housekeeping_log_room_room_type_id', 'housekeeping_log_employee_id'], 'exist', 'skipOnError' => true, 'targetClass' => HousekeepingLog::className(), 'targetAttribute' => ['housekeeping_log_id' => 'id', 'housekeeping_log_room_id' => 'room_id', 'housekeeping_log_room_room_type_id' => 'room_room_type_id', 'housekeeping_log_employee_id' => 'employee_id']],
         ];
     }
 
@@ -45,14 +53,18 @@ class HousekeepingLogDetails extends \yii\db\ActiveRecord
             'housekeeping_log_details_checklist' => 'Housekeeping Log Details Checklist',
             'housekeeping_log_details_status' => 'Housekeeping Log Details Status',
             'housekeeping_log_details_timecompleted' => 'Housekeeping Log Details Timecompleted',
+            'housekeeping_log_id' => 'Housekeeping Log ID',
+            'housekeeping_log_room_id' => 'Housekeeping Log Room ID',
+            'housekeeping_log_room_room_type_id' => 'Housekeeping Log Room Room Type ID',
+            'housekeeping_log_employee_id' => 'Housekeeping Log Employee ID',
         ];
     }
 
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getHousekeepingLogs()
+    public function getHousekeepingLog()
     {
-        return $this->hasMany(HousekeepingLog::className(), ['housekeeping_log_details_id' => 'id']);
+        return $this->hasOne(HousekeepingLog::className(), ['id' => 'housekeeping_log_id', 'room_id' => 'housekeeping_log_room_id', 'room_room_type_id' => 'housekeeping_log_room_room_type_id', 'employee_id' => 'housekeeping_log_employee_id']);
     }
 }
