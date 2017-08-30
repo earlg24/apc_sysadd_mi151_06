6ce7384ca9ce53628af9891bdeac8e1a2d9855cc
tubetita <tyronnebetita@gmail.com>
2017-08-30 14:09:19 +0800

    modified checklist-items/_form.php and checklist-items/index.php

--- Application/basic/views/checklist-items/_form.php
+++ Application/basic/views/checklist-items/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\RoomType;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\ChecklistItems */
@@ -14,7 +16,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'checklist_ref_id')->textInput() ?>
 
-    <?= $form->field($model, 'room_type_id')->textInput() ?>
+    <?= $form->field($model, 'room_type_id')->dropDownList(ArrayHelper::map(RoomType::find()->all(), 'id', 'id'), ['prompt' => 'Select RoomTypeID']) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
--- Application/basic/views/checklist-items/index.php
+++ Application/basic/views/checklist-items/index.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+            //'id',
             'checklist_ref_id',
             'room_type_id',
 
