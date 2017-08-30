35a049349de38cdcf7db83f632ea64b2a26c0632
tubetita <tyronnebetita@gmail.com>
2017-08-30 14:13:31 +0800

    modified checklist-items/_form.php

--- Application/basic/views/checklist-items/_form.php
+++ Application/basic/views/checklist-items/_form.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use app\models\RoomType;
+use app\models\ChecklistRef;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\ChecklistItems */
@@ -14,7 +15,7 @@ use app\models\RoomType;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'checklist_ref_id')->textInput() ?>
+    <?= $form->field($model, 'checklist_ref_id')->dropDownList(ArrayHelper::map(ChecklistRef::find()->all(), 'id', 'id'), ['prompt' => 'Select ChecklistRefID']) ?>
 
     <?= $form->field($model, 'room_type_id')->dropDownList(ArrayHelper::map(RoomType::find()->all(), 'id', 'id'), ['prompt' => 'Select RoomTypeID']) ?>
 
