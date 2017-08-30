66653b75593ad704e930c58cb4518bf250bc1b47
tubetita <tyronnebetita@gmail.com>
2017-08-30 14:06:23 +0800

    modified checklist-ref/_form.php and checklist-ref/index.php

--- Application/basic/views/checklist-ref/_form.php
+++ Application/basic/views/checklist-ref/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\ChecklistCategory;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\ChecklistRef */
@@ -12,9 +14,9 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'checklist_description')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'checklist_description')->textInput() ?>
 
-    <?= $form->field($model, 'checklist_category_id')->textInput() ?>
+    <?= $form->field($model, 'checklist_category_id')->dropDownList(ArrayHelper::map(ChecklistCategory::find()->all(), 'id', 'id'), ['prompt' => 'Select ChecklistCategory']) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
--- Application/basic/views/checklist-ref/index.php
+++ Application/basic/views/checklist-ref/index.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+            //'id',
             'checklist_description',
             'checklist_category_id',
 
