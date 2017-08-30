e8798ab1f1bb7fceb7e16074e418241859fb28b1
earlg24 <earlpetergonzaga2@gmail.com>
2017-08-30 14:55:05 +0800

    modified housekeeping-log-details/_form.php

--- Application/basic/views/housekeeping-log-details/_form.php
+++ Application/basic/views/housekeeping-log-details/_form.php
@@ -4,6 +4,9 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use app\models\HousekeepingLog;
+use app\models\Room2;
+use app\models\RoomType;
+use app\models\Employee4;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\HousekeepingLogDetails */
@@ -22,11 +25,11 @@ use app\models\HousekeepingLog;
 
     <?= $form->field($model, 'housekeeping_log_id')->dropDownList(ArrayHelper::map(HousekeepingLog::find()->all(), 'id', 'id'), ['prompt' => 'Select HousekeepingLogID']) ?>
 
-    <?= $form->field($model, 'housekeeping_log_room_id')->textInput() ?>
+    <?= $form->field($model, 'housekeeping_log_room_id')->dropDownList(ArrayHelper::map(Room2::find()->all(), 'id', 'id'), ['prompt' => 'Select RoomID']) ?>
 
-    <?= $form->field($model, 'housekeeping_log_room_room_type_id')->textInput() ?>
+    <?= $form->field($model, 'housekeeping_log_room_room_type_id')->dropDownList(ArrayHelper::map(RoomType::find()->all(), 'id', 'id'), ['prompt' => 'Select RoomTypeID']) ?>
 
-    <?= $form->field($model, 'housekeeping_log_employee_id')->textInput() ?>
+    <?= $form->field($model, 'housekeeping_log_employee_id')->dropDownList(ArrayHelper::map(Employee4::find()->all(), 'id', 'id'), ['prompt' => 'Select EmployeeID']) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
