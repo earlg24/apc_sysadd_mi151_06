d587a8e10f6c3f5e3e03e61e3e3f42bf797108ae
ryngrcia <ryngrcia@gmail.com>
2017-08-31 09:12:06 +0800

    modified employee4/_form.php

--- Application/basic/views/employee4/_form.php
+++ Application/basic/views/employee4/_form.php
@@ -18,7 +18,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'employee_mi')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'employee_position')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'employee_position')->dropDownList(['Inspector' => 'Inspector', 'Housekeeper' => 'Housekeeper',]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
